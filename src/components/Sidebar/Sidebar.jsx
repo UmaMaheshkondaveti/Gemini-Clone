78da6b3349e3e9b4b9aeff6a7d7d450083b8f9d8
venkata surya uma mahesh kondaveti <124602557+UmaMaheshkondaveti@users.noreply.github.com>
2025-04-26 13:26:34 +0530

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 
-import "./sidebar.css";
+import "./Sidebar.css";
 import { assets } from "../../assets/assets";
 import { useContext, useState } from "react";
 import { Context } from "../../context/Context";
